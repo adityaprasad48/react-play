35a4a7a7b8230cb140f176561f392056b40f0594
adityaprasad48 <adityaprasad20799@gmail.com>
2021-12-04 17:20:08 +0530

    old way  dispatch and slelecting state added

--- src/About.js
+++ src/About.js
@@ -1,16 +1,17 @@
 import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { connect } from 'react-redux';
 
-export const About = () => {
+const About = (props) => {
 	const [contact, setContact] = useState({
 		name: '',
 		age: '',
 	});
 
-	const isDone = useSelector((state) => state.contact.isDone);
-	const dispatch = useDispatch();
+	console.log({ props });
+	// const isDone = useSelector((state) => state.contact.isDone);
+	// const dispatch = useDispatch();
 
-	const state = useSelector((state) => state);
+	// const state = useSelector((state) => state);
 
 	const handleChange = (e) => {
 		setContact((pre) => ({
@@ -21,18 +22,9 @@ export const About = () => {
 
 	const handleSubmit = (e) => {
 		e.preventDefault();
-		dispatch({
-			type: 'CONTACT_ADDED',
-			payload: {
-				isDone: !isDone,
-			},
-		});
+		props.contactAdded(props.isDone);
 		console.log({ contact });
 	};
-
-	console.log({ state });
-
-	console.log({ isDone });
 
 	return (
 		<div>
@@ -60,7 +52,8 @@ export const About = () => {
 				</div>
 				<button type="submit">Submit</button>
 			</form>
-			{isDone ? (
+
+			{props.isDone ? (
 				<h1>Form is Submitted Successfully</h1>
 			) : (
 				<h1>Please Fill the Form</h1>
@@ -68,3 +61,47 @@ export const About = () => {
 		</div>
 	);
 };
+
+const mapStateToProps = (state) => ({ isDone: state.contact.isDone });
+
+// with Action creators
+// const mapDispatchToProps = {
+//   addTodo,
+//   deleteTodo,
+//   toggleTodo,
+// }
+
+const mapDispatchToProps = (dispatch) => {
+	return {
+		// dispatching plain actions
+		increment: () => dispatch({ type: 'INCREMENT' }),
+		decrement: () => dispatch({ type: 'DECREMENT' }),
+		reset: () => dispatch({ type: 'RESET' }),
+		contactAdded: (isDone) =>
+			dispatch({
+				type: 'CONTACT_ADDED',
+				payload: {
+					isDone: !isDone,
+				},
+			}),
+	};
+};
+
+// As the first argument passed in to connect , mapStateToProps is used for selecting the part of the data from the store that the connected component needs. It's frequently referred to as just mapState for short. It is called every time the store state changes.
+
+// export default MyComponent
+
+// Conventionally called mapDispatchToProps, this second parameter to connect() may either be an object, a function, or not supplied.
+
+// Your component will receive dispatch by default, i.e., when you do not supply a second parameter to connect():
+
+// connect methods provides dispatch method in component as props
+
+// export default connect()(About)
+
+// export default connect(mapStateToProps)(MyComponent);
+// export default connect(mapStateToProps, null)(MyComponent);
+
+// export default connect(null, mapDispatchToProps)(MyComponent);
+
+export default connect(mapStateToProps, mapDispatchToProps)(About);
